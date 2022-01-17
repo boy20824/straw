dbec314492207d321c9b082197b02cfaa5bd6eae
MSI\user <boy20824@gmail.com>
2022-01-18 00:28:24 +0800

    創建straw-search測試elasticsearch系統(本機端執行軟體)

---
+++ straw-search/src/main/java/cn/tedu/straw/search/StrawSearchApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.straw.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrawSearchApplication.class, args);
+    }
+
+}
